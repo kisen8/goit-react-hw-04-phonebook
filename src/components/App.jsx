71cfbb1a77bf482c3d6f8f00bc0e1df7488d1dd4
Ihor Komar <kisenbenz8@gmail.com>
2022-11-15 18:13:06 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+// import { useState, useEffect } from 'react';
+// import { nanoid } from 'nanoid';
+
 export const App = () => {
   return (
     <div
@@ -7,7 +10,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
